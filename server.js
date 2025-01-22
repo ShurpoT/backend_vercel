0b5cff4fce14c3a8ce422806615f5b10bef74b4c
ShurpoT <timashurpo@gmail.com>
2025-01-22 14:57:01 +0300

    changed server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+var cors = require("cors");
 const mysql = require("mysql2");
 const fs = require("fs");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/", express.static("public"));
 
